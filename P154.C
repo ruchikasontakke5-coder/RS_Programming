b657b183c657135a1688d894b5aa4f610f6ae88e
ruchikasontakke5-coder <ruchikasontakke5@gmail.com>
2025-10-03 22:50:30 +0530

    Create P154.C

---
+++ P154.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int size;
+    scanf("%d", &size);
+    for (int i = 1; i <= size; i++) {
+        for (int j = 1; j <= size; j++) {
+            if (i == 1 || i == size || j == 1 || j == size)
+                printf("* ");
+            else
+                printf("  ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
