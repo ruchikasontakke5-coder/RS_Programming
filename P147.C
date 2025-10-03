1742360bc841d9fe4b39bc99e5645b5542dcc5ae
ruchikasontakke5-coder <ruchikasontakke5@gmail.com>
2025-10-03 22:47:41 +0530

    Create P147.C

---
+++ P147.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int a, b, max;
+    scanf("%d %d", &a, &b);
+    max = (a > b) ? a : b;
+    for (;; max++) {
+        if (max % a == 0 && max % b == 0) {
+            printf("LCM = %d", max);
+            break;
+        }
+    }
+    return 0;
+}
