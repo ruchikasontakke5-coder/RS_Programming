b0958e0725ce8fd483c5fc33f66193c950c8819a
ruchikasontakke5-coder <ruchikasontakke5@gmail.com>
2025-10-03 22:47:08 +0530

    Create P146.C

---
+++ P146.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int a, b, gcd;
+    scanf("%d %d", &a, &b);
+    for (int i = 1; i <= a && i <= b; i++) {
+        if (a % i == 0 && b % i == 0)
+            gcd = i;
+    }
+    printf("GCD = %d", gcd);
+    return 0;
+}
