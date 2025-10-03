2ef8e55dbdf11b95e5078b89505c52dc72668776
ruchikasontakke5-coder <ruchikasontakke5@gmail.com>
2025-10-03 15:28:41 +0530

    Create P78.C

---
+++ P78.C
@@ -0,0 +1,11 @@
+// Program 78: Check if character is vowel or consonant
+#include <stdio.h>
+int main() {
+    char ch = 'e';
+    if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+       ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+        printf("Vowel");
+    else
+        printf("Consonant");
+    return 0;
+}
